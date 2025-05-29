9fb2326522781f476aebaca51ed4158132a67969
Alwin p abraham <alwinpabraham2004@gmail.com>
2025-05-29 06:12:21 +0530

    Made some changes in backend

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const cors = require('cors')
 const app = express()
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:process.env.FRONTEND_URL,
     credentials:true
 }))
 app.use(express.json())

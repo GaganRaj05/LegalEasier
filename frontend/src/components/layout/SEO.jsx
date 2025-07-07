e33fc04e3e3d6a04eee77d64511ec3f309291b5d
Alwin p abraham <alwinpabraham2004@gmail.com>
2025-07-08 02:55:20 +0530

    Added titles, meta tags, robots.txt for Google SEO pages

--- frontend/src/components/layout/SEO.jsx
+++ frontend/src/components/layout/SEO.jsx
@@ -13,7 +13,7 @@ const SEO = ({title, description, keywords, url, image}) => {
       <meta property="og:url" content={url} />
       <meta property="og:type" content="website" />
       
-
+      <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={image } />

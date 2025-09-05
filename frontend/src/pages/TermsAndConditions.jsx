76fb1edb1e1d62982eed8c3ae85ae18f5d20f26f
Lucifer <itzmegagan2@gmail.com>
2025-09-06 02:21:06 +0530

    Removed SEO component and updated robots.txt

--- frontend/src/pages/TermsAndConditions.jsx
+++ frontend/src/pages/TermsAndConditions.jsx
@@ -6,13 +6,7 @@ import SEO from "../components/layout/SEO";
 const TermsAndConditions = () => {
   return (
     <>
-      <SEO
-        title="Terms and Conditions - LegalEasier Legal Document Services"
-        description="Terms and conditions for LegalEasier legal document preparation services. Information about SMS marketing, privacy, and service usage terms."
-        keywords="terms and conditions, legal document service terms, SMS marketing terms, privacy policy, service agreement, legal terms, document preparation terms"
-        image="https://media.istockphoto.com/id/1449334081/photo/statue-of-lady-justice-on-desk-of-a-judge-or-lawyer.jpg?s=612x612&w=0&k=20&c=139ZS1ycMRXBqnPEWV3l08zBLNe40WPiAudVnmeQrl8="
-        url="https://www.legaleasier.org/legal-easier/Terms&Conditions"
-      />
+      
 
       <NavBar />
       <ReUsableBanner bannerType="Terms and Conditions" />
